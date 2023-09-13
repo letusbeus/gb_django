3a28acb60682b341eee26074226eebf6a1dfeae5
letusbeus <letusbeus@mail.ru>
2023-09-14 01:50:52 +0300

    HW_3: added the following commands: display unique products purchased by the specific customer over the last year, last month & last week

---
+++ home_project/hw_2/management/commands/order_for_365_days.py
@@ -0,0 +1,22 @@
+from datetime import timedelta, datetime
+from django.core.management.base import BaseCommand
+from hw_2.models import Order, Product
+
+
+class Command(BaseCommand):
+    help = 'Get products from a specific customer\'s orders for the last 365 days.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('customer', type=str, help='Customer ID')
+
+    def handle(self, *args, **kwargs):
+        customer = kwargs['customer']
+        year_ago = datetime.now() - timedelta(days=3650)
+        orders = Order.objects.filter(customer=customer, placed__gte=year_ago).order_by('-placed')
+
+        if orders.exists():
+            products = Product.objects.filter(order__in=orders).distinct()
+            result = '\n'.join(f'{i + 1}. {product}' for i, product in enumerate(products))
+            self.stdout.write(result)
+        else:
+            self.stdout.write(f'Customer ID {customer} has not placed any orders in the last 365 days.')
